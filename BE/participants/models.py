336b6970c5c1ac73fb22928db24d2374f37614fc
이은찬 <jsmisth26@gmail.com>
2025-05-09 19:18:52 +0900

    feat: participants models.py complete #6

--- BE/participants/models.py
+++ BE/participants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from schedules.models import Schedules
 
 # Create your models here.
+class Participants(models.Model):
+    meal_schedule = models.ForeignKey(Schedules, on_delete=models.CASCADE)
+    participants = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete= models.SET_NULL,
+        null=True,
+        blank=True
+    )
