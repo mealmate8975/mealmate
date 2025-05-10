aaf73b5f4b6204e7b1400899822c0a7f5b8b678f
goldangeee <goldangeee@proton.me>
2025-05-10 23:02:19 +0900

    feat: implement friend request acceptance with tests

--- BE/friendships/tests.py
+++ BE/friendships/tests.py
@@ -34,3 +34,36 @@ class SendFriendRequestTest(APITestCase):  # APITestCase를 상속받아 테스�
         
         # 첫 번째 사용자와 두 번째 사용자 간의 친구 요청이 데이터베이스에 존재하는지 확인
         self.assertTrue(Friendship.objects.filter(from_user=self.user1, to_user=self.user2).exists())
+
+class AcceptFriendRequestTest(APITestCase):
+    def setUp(self):
+        # 첫 번째 사용자 생성
+        self.user1 = User(email="user1@example.com", name="User One", nickname="user1", gender='0')  # User 객체 생성
+        self.user1.set_password("pass")  # 비밀번호를 해시화하여 설정
+        self.user1.save()  # 사용자 객체를 데이터베이스에 저장
+
+        # 두 번째 사용자 생성
+        self.user2 = User(email="user2@example.com", name="User Two", nickname="user2", gender='1')
+        self.user2.set_password("pass")
+        self.user2.save()
+
+        # 친구요청 생성
+        self.friendrequest = Friendship(from_user=self.user1,to_user=self.user2,status='pending')
+        self.friendrequest.save()
+
+        self.client.force_login(self.user2)  # 테스트 클라이언트를 사용하여 두 번째 사용자를 강제 로그인 상태로
+
+    def test_accept_friend_request(self):  # 친구 요청 수락 기능을 테스트하는 메서드
+        url = reverse('friendship-accept')  # 'friendship-accept'라는 이름의 URL을 역으로 찾기
+
+        # 친구 요청을 수락하는 POST 요청을 보냄
+        response = self.client.post(url, {'friendship_id': self.friendrequest.id}) # friendrequest.id를 포함하여 POST 요청을 보냄
+        
+        # 응답 상태 코드가 200인지 확인
+        self.assertEqual(response.status_code, 200)
+        
+        # 응답 데이터에서 상태 메시지가 '친구 요청 수락'인지 확인
+        self.assertEqual(response.data['status'], '친구 요청 수락')
+        
+        # 친구 요청이 수락이 데이터베이스에 반영됐는지 확인        
+        self.assertEqual(Friendship.objects.get(from_user=self.user1, to_user=self.user2).status,'accepted')
--- BE/friendships/views.py
+++ BE/friendships/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from .models import Friendship
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 User = get_user_model()
 
@@ -10,7 +11,7 @@ class SendFriendRequestView(APIView):
     permission_classes = [permissions.IsAuthenticated]  # 이 뷰에 접근하기 위해 사용자가 인증되어야 함을 설정
 
     def post(self, request):  # POST 요청을 처리하는 메서드
-        to_user_id = request.data.get('to_user_id')  # 요청 데이터에서 'to_user_id'를 가져오기
+        to_user_id = request.data.get('to_user_id')  # 요청 데이터에서 'to_user_id' 가져오기
         
         # 주어진 ID로 사용자를 데이터베이스에서 검색
         to_user = User.objects.get(id=to_user_id)  # ID에 해당하는 사용자 객체를 가져오기
@@ -18,5 +19,26 @@ class SendFriendRequestView(APIView):
         # Friendship 모델을 사용하여 친구 요청을 생성
         Friendship.objects.create(from_user=request.user, to_user=to_user, status='pending')  # 현재 로그인한 사용자(request.user)와 대상 사용자(to_user) 간의 친구 요청을 생성
         
-        # 친구 요청이 성공적으로 생성되었음을 나타내는 응답을 반환
+        # 친구 요청이 성공적으로 생성되었음을 나타내는 응답 반환
         return Response({'status': '요청 보냄'}, status=201) # HTTP 상태 코드는 201(Created)
+
+class AcceptFriendRequestView(APIView):
+    permission_classes = [permissions.IsAuthenticated]  # 이 뷰에 접근하기 위해 사용자가 인증되어야 함을 설정
+
+    def post(self, request): # POST 요청을 처리하는 메서드
+        friendship_id = request.data.get('friendship_id') # 요청 데이터에서 'friendship_id' 가져오기
+
+        # 친구요청 수신자가 현재 로그인한 유저이고 friendship_id가 일치하는 친구요청 객체 가져오기
+        friendship = get_object_or_404(Friendship,to_user=request.user,id=friendship_id) 
+        
+        # 중복 수락 방지 로직
+        if friendship.status == 'accepted':
+            return Response({'error': '이미 수락된 요청입니다.'}, status=400)
+        
+        friendship.status = 'accepted' 
+        friendship.save()
+
+        # 친구 요청이 pending에서 accepted로 성공적으로 수정되었다는 응답 반환
+        # HTTP 상태 코드는 200(OK) 요청이 성공적으로 처리되었음
+        return Response({'status': '친구 요청 수락'}, status=200) 
+
