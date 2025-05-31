c16c7b197b2951d3bae6d38f62c5086f9d55b15d
goldangeee <goldangeee@proton.me>
2025-05-31 23:57:47 +0900

    feat: 호스트와 게스트 기반 스케줄 ID 산출 기능 추가

--- BE/schedules/schedule_service.py
+++ BE/schedules/schedule_service.py
@@ -45,25 +45,42 @@ class ScheduleService:
         schedule = get_object_or_404(Schedules, pk=pk, created_by=user)
         schedule.delete()
 
+    # participant = host + guest
+
     # (호스트의 id + 게스트들의 id) 추출
     @staticmethod
-    def get_all_related_user_ids(pk,user): # 해당 스케줄의 pk와 request.user(생성자이자 호스트)를 통해 스케줄 호스트와 게스트들의 id를 찾아서 리턴하는 함수
+    def get_participant_user_ids(pk,user): # 해당 스케줄의 pk와 request.user(생성자이자 호스트)를 통해 스케줄 호스트와 게스트들의 id를 찾아서 리턴하는 함수
         target_schedule = get_object_or_404(Schedules,pk=pk)
 
-        if user.id != target_schedule.created_by.id: # user가 pk로 찾은 스케줄의 생성자와 일치하는지 확인하는 로직
+        if user.id != target_schedule.created_by.id: # user.id가 pk로 찾은 스케줄의 생성자 id와 일치하는지 확인하는 로직
             raise PermissionDenied("해당 스케줄에 대한 권한이 없습니다.")
         
-        participant_id_list = list(
-        Participants.objects.filter(schedule=target_schedule.id).values_list("participants_id", flat=True)
-        )
+        participant_id_list = list(Participants.objects.filter(schedule=target_schedule).values_list("participants_id", flat=True).distinct())
         participant_id_list.append(user.id)
-        return list(set(participant_id_list))
 
-    # 참여자로 스케줄 찾기
-    # (호스트의 id + 게스트들의 id)로 participants 테이블에서 스케줄 id 추출(중복제거)
+        return participant_id_list
 
-    # 생성자로 스케줄 찾기
-    # (호스트의 id + 게스트들의 id)로 schedules 테이블에서 생성자로 스케줄 id 추출
+    @staticmethod
+    def get_related_schedule_ids_by_user_ids(pk, user):
+        participant_id_list = ScheduleService.get_participant_user_ids(pk, user)
+
+        # 참여자로 스케줄 찾기
+        # (호스트의 id + 게스트들의 id)로 participants 테이블에서 스케줄 id 추출(중복제거)
+        guest_schedule_ids = list(Participants.objects.filter(
+            participant__in=participant_id_list
+        ).values_list("schedule", flat=True).distinct())
+
+        # 생성자로 스케줄 찾기
+        # (호스트의 id + 게스트들의 id)로 schedules 테이블에서 생성자로 스케줄 id 추출
+        host_schedule_ids = list(Schedules.objects.filter(
+            created_by__in=participant_id_list
+        ).values_list("schedule_id", flat=True).distinct())
+
+        return list(set(guest_schedule_ids + host_schedule_ids))
+    
+    # 데이터가 많을 때 성능 저하를 방지하기 위해서 리스트를 사용하지 않는 방식을 고민해보기
+
+        
 
     # 스케줄 id 중복제거하기
 
