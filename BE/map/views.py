0706afd3b0cef417b718d67613dcb7d6b9bdd62e
goldangeee <goldangeee@proton.me>
2025-07-06 17:14:21 +0900

    chore : 지도에 약속 장소와 나의 실시간 위치 표시 성공

--- BE/map/views.py
+++ BE/map/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from schedules.models import Schedules
+import json
 
 def map_view(request):
-    return render(request, 'map/map.html')
+    schedule_id = request.GET.get("schedule_id")
+
+    # 1. schedule_id 없을 경우 폼만 보여줌
+    if not schedule_id:
+        return render(request, 'map/map_form.html')
+    
+    # 2. 스케줄 조회
+    schedule = get_object_or_404(Schedules, schedule_id=schedule_id)
+    coord = schedule.schedule_condition or {}
+
+    
+    # 3. 좌표 추출 (예외 방지용 기본값 처리)
+    try:
+        coords = {
+            "lat": coord["latitude"],
+            "lng": coord["longitude"]
+        }
+    except KeyError:
+        coords = {
+            "lat": 37.5665,
+            "lng": 126.9780
+        }
+    return render(request, 'map/map.html', {
+    "coords": json.dumps(coords)
+    })
