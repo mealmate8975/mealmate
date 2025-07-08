a79290bc6bc846ab0973fd589e55307c44ce98b9
goldangeee <goldangeee@proton.me>
2025-07-08 23:14:42 +0900

    feat : update_real_time_location 메서드 구현

--- BE/map/map_service.py
+++ BE/map/map_service.py
@@ -7,34 +7,48 @@ map_service.py
 views.py
 클라이언트의 HTTP 요청을 받고, 인증과 응답 처리만 담당하는 컨트롤러 역할의 뷰 레이어
 '''
+from .models import RealTimeLocation
+from django.db import transaction
+from django.shortcuts import get_object_or_404
 
-class MapSerive:
-    def update_coords():
-        pass
+class RealTimeLocationService:
+    @staticmethod
+    def update_real_time_location(user, latitude, longitude):
+        with transaction.atomic():
+            instance = RealTimeLocation.objects.filter(user=user).first()
+            if instance:
+                instance.latitude = latitude
+                instance.longitude = longitude
+                instance.save()
+                return True, "Coords updated successfully."
+            else:
+                RealTimeLocation.objects.create(user=user, latitude=latitude, longitude=longitude)
+                return True, "Coords created successfully."
 
-def map_view(request):
-    schedule_id = request.GET.get("schedule_id")
 
-    # 1. schedule_id 없을 경우 폼만 보여줌
-    if not schedule_id:
-        return render(request, 'map/map_form.html')
+# def map_view(request):
+#     schedule_id = request.GET.get("schedule_id")
+
+#     # 1. schedule_id 없을 경우 폼만 보여줌
+#     if not schedule_id:
+#         return render(request, 'map/map_form.html')
     
-    # 2. 스케줄 조회
-    schedule = get_object_or_404(Schedules, schedule_id=schedule_id)
-    coord = schedule.schedule_condition or {}
+#     # 2. 스케줄 조회
+#     schedule = get_object_or_404(Schedules, schedule_id=schedule_id)
+#     coord = schedule.schedule_condition or {}
 
     
-    # 3. 좌표 추출 (예외 방지용 기본값 처리)
-    try:
-        coords = {
-            "lat": coord["latitude"],
-            "lng": coord["longitude"]
-        }
-    except KeyError:
-        coords = {
-            "lat": 37.5665,
-            "lng": 126.9780
-        }
-    return render(request, 'map/map.html', {
-    "coords": json.dumps(coords)
-    })
+#     # 3. 좌표 추출 (예외 방지용 기본값 처리)
+#     try:
+#         coords = {
+#             "lat": coord["latitude"],
+#             "lng": coord["longitude"]
+#         }
+#     except KeyError:
+#         coords = {
+#             "lat": 37.5665,
+#             "lng": 126.9780
+#         }
+#     return render(request, 'map/map.html', {
+#     "coords": json.dumps(coords)
+#     })
