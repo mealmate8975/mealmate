d3d885c2b5bcc62063a00ab040c2ba067f439ae9
goldangeee <goldangeee@proton.me>
2025-07-24 08:54:10 +0900

    feat : 포스트 작성 테스트 성공

--- BE/posts/urls.py
+++ BE/posts/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from .views import (
     feed_view,
-    toggle_like,    
+    toggle_like,
+    create_post_view,    
 )
 
 app_name = "posts"
 
 urlpatterns = [
-    path('feed/', feed_view, name='feed_view'),      #전체 포스트 피드
-    # path('', ?.as_view(), name='?'),      # /<id>/     게시글 상세
+    path('feed/', feed_view, name='feed_view'),                         #전체 포스트 피드
+    # path('', ?.as_view(), name='?'),                                  # /<id>/     게시글 상세
     path('like/<int:post_id>/', toggle_like, name='toggle_like'),
-    path('like/<int:post_id>/', toggle_like, name='toggle_like'),
+    path('new', create_post_view, name='create_post_view'),
 ]
