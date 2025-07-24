4636de3247033175f44dda28fb366b534677bd2c
goldangeee <goldangeee@proton.me>
2025-07-24 09:05:20 +0900

    feat : 페이지 상세보기 기능 성공

--- BE/pages/urls.py
+++ BE/pages/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-# from .views import (
+from .views import (
 #     pagelist_view,
-#     page_view,
-# )
+    page_detail,
+)
 from posts.views import create_post_view
 
 app_name = "pages"
 
 urlpatterns = [
     # path('pagelist/', pagelist_view, name='pagelist_view'),                     # 페이지 리스트
-    # path('<int:id>/', page_view, name='page_view'),                             # 페이지 상세 정보 + 게시판
+    path('<int:page_id>/', page_detail, name='page_detail'),                             # 페이지 상세 정보 + 게시판
     # path('<int:id>/post/new',page_new_post_view , name='page_new_post_view'),   # 페이지에 게시글 작성
 ]
