860cb87ffe979bdd893aff8f935e0a1e37dab5e7
goldangeee <goldangeee@proton.me>
2025-07-28 14:07:35 +0900

    test : test_authenticated_user_can_update_post까지 완료

--- BE/posts/tests.py
+++ BE/posts/tests.py
@@ -106,36 +106,71 @@ class PostTestBase(APITestCase):
 #         self.assertEqual(response.status_code,401)
 #         # 이미지 파일 테스트도 필요
 
-class TestPostListAPIView(PostTestBase):
+# class TestPostListAPIView(PostTestBase):
+#     def setUp(self):
+#         super().setUp()
+
+#         self.page1 = Page.objects.create(
+#             name = "테스트 페이지1",
+#             description = "테스트 페이지1입니다.",
+#         )
+
+#         self.page2 = Page.objects.create(
+#             name = "테스트 페이지2",
+#             description = "테스트 페이지2입니다.",
+#         )
+    
+#     def test_post_list_returns_401_for_unauthenticated_user(self):
+#         url = reverse("posts:post_list")
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code,401)
+    
+#     def test_post_list_returns_empty_list_when_no_posts_exist(self):
+#         self.client.force_login(self.user1)
+#         url = reverse("posts:post_list")
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code,200)
+#         self.assertEqual(response.data, [])
+
+#     def test_post_list_returns_all_posts_for_authenticated_user(self):
+#         self.user2 = User(email="user2@example.com", name="User One", nickname="user2", gender='1')
+#         self.user2.set_password("pass")
+#         self.user2.save()
+
+#         self.post1 = Post.objects.create(
+#             author = self.user1,
+#             title = self.title,
+#             content = self.content,
+#             type = self.type_,
+#         )
+
+#         self.post2 = Post.objects.create(
+#             author = self.user2,
+#             page_id = self.page1.id,
+#             title = "테스트 포스트2",
+#             content = "테스트 포스트2입니다.",
+#             type = "review",
+#         )
+
+#         self.post3 = Post.objects.create(
+#             author = self.user1,
+#             page_id = self.page2.id,
+#             title = "테스트 포스트3",
+#             content = "테스트 포스트3입니다.",
+#             type = "tip",
+#         )
+
+#         self.client.force_login(self.user1)
+#         url = reverse("posts:post_list")
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code,200)
+#         self.assertEqual(len(response.data),3)
+#         self.assertEqual(response.data[0]["title"], self.post3.title)  # 최신순 검증
+#         self.assertEqual(response.data[-1]["title"], self.post1.title)
+
+class TestPostUpdateAPIView(PostTestBase):
     def setUp(self):
         super().setUp()
-
-        self.page1 = Page.objects.create(
-            name = "테스트 페이지1",
-            description = "테스트 페이지1입니다.",
-        )
-
-        self.page2 = Page.objects.create(
-            name = "테스트 페이지2",
-            description = "테스트 페이지2입니다.",
-        )
-    
-    def test_post_list_returns_401_for_unauthenticated_user(self):
-        url = reverse("posts:post_list")
-        response = self.client.get(url)
-        self.assertEqual(response.status_code,401)
-    
-    def test_post_list_returns_empty_list_when_no_posts_exist(self):
-        self.client.force_login(self.user1)
-        url = reverse("posts:post_list")
-        response = self.client.get(url)
-        self.assertEqual(response.status_code,200)
-        self.assertEqual(response.data, [])
-
-    def test_post_list_returns_all_posts_for_authenticated_user(self):
-        self.user2 = User(email="user2@example.com", name="User One", nickname="user2", gender='1')
-        self.user2.set_password("pass")
-        self.user2.save()
 
         self.post1 = Post.objects.create(
             author = self.user1,
@@ -144,26 +179,33 @@ class TestPostListAPIView(PostTestBase):
             type = self.type_,
         )
 
-        self.post2 = Post.objects.create(
-            author = self.user2,
-            page_id = self.page1.id,
-            title = "테스트 포스트2",
-            content = "테스트 포스트2입니다.",
-            type = "review",
-        )
+        self.title2 = "포스트 수정 테스트1"
+        self.content2 = "포스트 수정 테스트입니다."
+        self.type_2 = "tip"
 
-        self.post3 = Post.objects.create(
-            author = self.user1,
-            page_id = self.page2.id,
-            title = "테스트 포스트3",
-            content = "테스트 포스트3입니다.",
-            type = "tip",
-        )
-
+#     # 실패 케이스 (UnHappy Path) 
+#     def test_post_update_for_unauthenticated_user(self):
+#         url = reverse("posts:post_update",kwargs={"pk":self.post1.id})
+#         data = {
+#             "title" : self.title2,
+#             "content" :self.content2,
+#             "type" : self.type_2, 
+#         }
+#         response = self.client.patch(url,data,format='json')
+#         self.assertEqual(response.status_code,401)
+    
+    # 성공 케이스 (Happy Path)
+    def test_authenticated_user_can_update_post(self):
         self.client.force_login(self.user1)
-        url = reverse("posts:post_list")
-        response = self.client.get(url)
+        url = reverse("posts:post_update",kwargs={"pk":self.post1.id})
+        data = {
+            "title" : self.title2,
+            "content" :self.content2,
+            "type" : self.type_2, 
+        }
+        response = self.client.patch(url,data,format='json')
         self.assertEqual(response.status_code,200)
-        self.assertEqual(len(response.data),3)
-        self.assertEqual(response.data[0]["title"], self.post3.title)  # 최신순 검증
-        self.assertEqual(response.data[-1]["title"], self.post1.title)
+        self.post1.refresh_from_db() # 값 업데이트
+        self.assertEqual(self.post1.title, self.title2)
+        self.assertEqual(self.post1.content, self.content2)
+        self.assertEqual(self.post1.type, self.type_2)
