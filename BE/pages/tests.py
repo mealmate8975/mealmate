f68087ac8de7cf44721d3ae17f675ebd8754ed7a
goldangeee <goldangeee@proton.me>
2025-07-29 17:02:24 +0900

    refactor : 커뮤니티 기능 DRF 스타일로 변경 완료

--- BE/pages/tests.py
+++ BE/pages/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from posts.models import Post
+from pages.models import Page
+
+User = get_user_model()
+
+class TestPageDetailAPIView(APITestCase):
+    def setUp(self):
+        self.user1 = User(email="user1@example.com", name="User One", nickname="user1", gender='0')
+        self.user1.set_password("pass")
+        self.user1.save()
+
+        self.user2 = User(email="user2@example.com", name="User Two", nickname="user2", gender='1')
+        self.user2.set_password("pass")
+        self.user2.save()
+
+        self.page = Page.objects.create(
+            name = "테스트 페이지1",
+            description = "테스트 페이지1의 description입니다.",
+        )
+
+        self.post1 = Post.objects.create(
+            author = self.user1,
+            title = "포스트1",
+            content = "포스트1의 콘텐트입니다",
+            type = "review",
+            page = self.page,
+        )
+
+        self.post2 = Post.objects.create(
+            author = self.user2,
+            title = "포스트2",
+            content = "포스트2의 콘텐트입니다",
+            type = "tip",
+            page = self.page,
+        )
+   
+    # 성공 테스트
+    def test_page_detail_includes_ordered_posts(self):
+        url = reverse("pages:page-detail",kwargs={"pk":self.page.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(len(response.data["posts"]),2)
+        self.assertEqual(response.data["posts"][0]["title"], "포스트2")
+    
+    def test_page_detail_returns_empty_posts_when_none_exist(self):
+        '''
+        정상 동작에 대한 경계 상황(edge case) 테스트
+        '''
+        page_without_posts = Page.objects.create(
+            name="빈 페이지",
+            description = "빈 페이지의 description입니다.",
+        )
+        url = reverse("pages:page-detail", kwargs={"pk": page_without_posts.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["posts"], [])
+    
+    # 실패 테스트
+    def test_page_detail_returns_404_for_nonexistent_page(self):
+        url = reverse("pages:page-detail", kwargs={"pk": 3}) # 앞 테스트의 Page(pk=2)는 삭제되었지만, pk 값 증가흔적은 다음 테스트에 영향을 줌
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    
--- BE/pages/urls.py
+++ BE/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
 #     pagelist_view,
-    page_detail,
+    # page_detail,
+    PageDetailAPIView,
 )
 # from posts.views import create_post_view
 
@@ -9,6 +10,6 @@ app_name = "pages"
 
 urlpatterns = [
     # path('pagelist/', pagelist_view, name='pagelist_view'),                     # 페이지 리스트
-    path('<int:page_id>/', page_detail, name='page_detail'),                # 페이지 상세 정보 + 게시판
+    path('<int:pk>/', PageDetailAPIView.as_view(), name='page-detail'),                # 페이지 상세 정보 + 게시판
     # path('<int:page_id>/post/new',create_post_view , name='create_post_view'),   # 페이지에 게시글 작성
 ]
