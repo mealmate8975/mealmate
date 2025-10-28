568eb486ca764744402b1172678f282b9242bf6b
goldangeee <goldangeee@proton.me>
2025-10-28 17:58:52 +0900

    refactor : 약속 시작/종료 시간 세팅(변경) 기능 리팩토링 진행 중

--- BE/schedules/schedule_service.py
+++ BE/schedules/schedule_service.py
@@ -52,18 +52,18 @@ class ScheduleQueryService:
         schedule = get_object_or_404(Schedules, pk=pk, created_by=user)
         return ScheduleSerializer(schedule).data   
 
-    # participant = host + guest
-    # (호스트의 id + 게스트들의 id) 추출
     @staticmethod
-    def get_participant_user_ids(pk,user): 
+    def get_participant_user_ids(schedule_pk,user): 
         '''
         해당 스케줄의 pk와 
-        request.user(생성자이자 호스트)를 통해 
+        request.user를 통해 
         스케줄 호스트와 게스트들의 id를 찾아서 리턴하는 함수
-        '''
-        target_schedule = get_object_or_404(Schedules,pk=pk)
 
-        if user.id != target_schedule.created_by.id: # user.id가 pk로 찾은 스케줄의 생성자 id와 일치하는지 확인하는 로직
+        participant = host + guest
+        '''
+        target_schedule = get_object_or_404(Schedules,pk=schedule_pk)
+
+        if user.id != target_schedule.created_by.id: # user.id가 pk로 찾은 스케줄의 생성자 id와 일치하는지 확인
             raise PermissionDenied("해당 스케줄에 대한 권한이 없습니다.")
 
         guest_id_list = list(Participants.objects.filter(schedule=target_schedule).values_list("participant_id", flat=True).distinct())
@@ -96,39 +96,37 @@ class ScheduleQueryService:
         return combined_schedule_ids
 
     @staticmethod
-    def get_schedules_in_month_range(pk, user,new_schedule_start,new_schedule_end):
+    def get_schedules_in_range(schedule_pk, user,new_schedule_start,new_schedule_end):
         '''
-        스케줄 id로 스케줄 테이블에서 해당하는 달에 속해있는 약속들 찾기
+        새로운 스케줄과 겹치는 모든 스케줄을 찾는 메서드
         '''        
-        related_schedule_id_set = ScheduleQueryService.get_related_schedule_ids_by_user_ids(pk,user)
+        # related_schedule_id_set = ScheduleQueryService.get_related_schedule_ids_by_user_ids(scheduel_pk,user)
 
-        new_schedule_start_month = new_schedule_start.month
-        new_schedule_end_month = new_schedule_end.month
-
-        new_schedule_month_range = range(new_schedule_start_month,new_schedule_end_month+1)
-        schedules_in_month_range = Schedules.objects.filter(schedule_id__in=related_schedule_id_set).filter(Q(schedule_start__month__in=new_schedule_month_range) | Q(schedule_end__month__in=new_schedule_month_range)).distinct()
+        # new_schedule_month_range = range(new_schedule_start_month,new_schedule_end_month+1)
+        # schedules_in_month_range = Schedules.objects.filter(schedule_id__in=related_schedule_id_set).filter(Q(schedule_start__month__in=new_schedule_month_range) | Q(schedule_end__month__in=new_schedule_month_range)).distinct()
         
-        return schedules_in_month_range
-    # schedules_in_month_range을 프론트로 보내줘서 새로운 스케줄의 시작과 끝 시간을 선택할 때 시각적으로 표현해줘야함
+        # return 
+
 class ScheduleTimeService:
-    # 모두가 가능한 시간대 고르기
     @staticmethod
-    def select_available_time(pk, user,new_schedule_start,new_schedule_end):
+    def select_available_time(schedule_pk,host,new_schedule_start,new_schedule_end):
+        '''
+        모두가 가능한 시간대 고르기
+        '''
         serializer = ScheduleSerializer(data={"schedule_start": new_schedule_start, "schedule_end": new_schedule_end}, partial=True)
         serializer.is_valid(raise_exception=True)
 
         new_schedule_start = serializer.validated_data["schedule_start"]
         new_schedule_end = serializer.validated_data["schedule_end"]
 
-        schedules_in_month_range = ScheduleQueryService.get_schedules_in_month_range(pk, user,new_schedule_start,new_schedule_end)
-
         # 기존 약속과 충돌하는 새로운 약속일 경우 에러 발생시키기
-        if schedules_in_month_range.filter(schedule_end__gte=new_schedule_start,schedule_start__lte=new_schedule_end).exists():
-            raise ValidationError("기존 약속과 충돌하는 새로운 약속입니다.")
+
+        # if ?.filter(schedule_end__gte=new_schedule_start,schedule_start__lte=new_schedule_end).exists():
+        #     raise ValidationError("기존 약속과 충돌하는 새로운 약속입니다.")
 
         data = {
             "schedule_start": new_schedule_start,
             "schedule_end": new_schedule_end,
         }
 
-        return ScheduleCommandService.update_schedule(pk, user, data)
+        return ScheduleCommandService.update_schedule(schedule_pk, host, data)
